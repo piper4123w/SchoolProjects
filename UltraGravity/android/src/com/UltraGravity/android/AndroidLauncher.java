d6dc89aa2a8b3cb5de1c23a22dc213d24db36716
piper4123w <kyle.lawson@marquette.edu>
2014-09-18 21:27:53 -0500

    First Commit of Empty Core from libGDX

---
+++ UltraGravity/android/src/com/UltraGravity/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.UltraGravity.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.UltraGravity.UltraGravity;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new UltraGravity(), config);
+	}
+}
---
+++ UltraGravity/desktop/src/com/UltraGravity/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.UltraGravity.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.UltraGravity.UltraGravity;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new UltraGravity(), config);
+	}
+}
