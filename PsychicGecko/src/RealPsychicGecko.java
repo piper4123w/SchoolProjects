3f8ce32b520d18217d74ac70bb24c2188c45982e
piper4123w <kyle.lawson@marquette.edu>
2014-09-25 16:43:46 -0500

    Fix needed in beanIsNeighboringSecondGecko 	path is discovered through desired stop point

---
+++ PsychicGecko/src/RealPsychicGecko.java
@@ -0,0 +1,15 @@
+import info.gridworld.actor.Actor;
+import info.gridworld.actor.Critter;
+import info.gridworld.grid.Location;
+import info.gridworld.grid.Grid;
+import info.gridworld.actor.Flower;
+
+import java.util.ArrayList;
+
+public class RealPsychicGecko extends Critter{
+
+  public RealPsychicGecko() {
+    // TODO Auto-generated constructor stub
+  }
+
+}
